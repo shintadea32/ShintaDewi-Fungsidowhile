cf23a492c0e426466ad86bb3fdecc625422abede
shintadewi29rpl <shinta_dewi_29rpl@gmail.com>
2021-01-15 00:05:04 +0700

    Project Ke Tujuh

---
+++ src/fungsi/pkgdo/pkgwhile/FungsiDoWhile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fungsi.pkgdo.pkgwhile;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class FungsiDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil=1;
+        do{
+        //statement
+        System.out.print(bil+" ");
+        bil++;
+        }while (bil<=10);       
+        }
+    }
+    
+}
